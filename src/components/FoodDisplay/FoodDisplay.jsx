97c4bd7013738138ac853189eaab198eb2024283
nusrat jahan <nusratjahanlima639@gmail.com>
2024-05-07 20:30:08 +0600

    food category done

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -12,7 +12,9 @@ const FoodDisplay = ({category}) => {
             <h2>Top dishesh for you</h2>
             <div className="food-display-list">
                 {food_list.map((item,index)=>{
-                    return <FoodItem key={index} id={item.name} name={item.name} description={item.description} price={item.price} image={item.image}/>
+                    if(category==="All" || category===item.category){
+                        return <FoodItem key={index} id={item.name} name={item.name} description={item.description} price={item.price} image={item.image}/>
+                    }
                 })}
             </div>
         </div>
